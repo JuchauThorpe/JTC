67f4b9384ca148816deb597a57332651067994b5
Juchau Thorpe <juchauthorpe@gmail.com>
2026-07-08 10:09:31 +0000

    Minor amendment to who we are

--- frontend/src/pages/WhoWeAre.jsx
+++ frontend/src/pages/WhoWeAre.jsx
@@ -2,7 +2,9 @@ export default function WhoWeAre() {
   return (
     <div className="page">
       <h2>Who We Are</h2>
-      <p>Juchau-Thorpe Consulting provides IT solutions and consultancy to small to medium sized businesses within the UK, utilising over 35 years of experience.</p>
+      <p>Juchau-Thorpe brings together many years of expertise in the fields of database application development and support. Drawing on our pool of consultants we are able to offer a wide range of consultation, software development and support services.</p>
+      <p>Our sole aim is to provide practical IT solutions for our clients. Using predominantly Microsoft products, everything from minor support of departmental databases, client/server applications and web enabled systems can be catered for.</p>
+      <p>Juchau-Thorpe has been founded over four decades of IT development in industries such as manufacturing, financial services, the public sector and publishing.</p>
     </div>
   );
 }
